965d3429a807e743d20842c2d4eb8f754508361a
peter <test.pd606@gmail.com>
2023-10-03 16:08:26 +0800

    LOTTIE動畫實裝網頁測試

---
+++ script/style.js
@@ -0,0 +1,39 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie'),
+    path: 'img/spooky-cat.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,    
+})
+
+var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie2'),
+    path: 'img/Animation - 1696228011624.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,    
+})
+
+var animation3 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie3'),
+    path: 'img/Animation - 1696313551123.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,    
+})
+
+var animation4 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie4'),
+    path: 'img/Animation - 1696313821153.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,    
+})
+
+var animation5 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie5'),
+    path: 'img/connection-error.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,    
+})
